a647d6951e6cd6d4cd3420e054f10e0998caa67e
DominiqueOyco <dominickoyco@gmail.com>
2024-10-01 13:48:24 -0700

    Added lesson 4

---
+++ lessons/lesson4.js
@@ -0,0 +1,30 @@
+//Relational or comparison operators
+
+// > - more than
+// < - less than
+// >= - more than equal 
+// <= - less than equal
+
+var result = 10 > 5
+console.log(result)
+var result = 4 > 5
+console.log(result)
+var result = 4 < 5
+console.log(result)
+var result = 5 > 5
+console.log(result)
+var result = 5 > 5
+console.log(result)
+var result = 5 >= 5
+console.log(result)
+var result = 6 >= 5
+console.log(result)
+var result = 4 >= 5
+console.log(result)
+var result = 4 <= 5
+console.log(result)
+
+//Equality operators
+var x = 1
+console.log(x == '1') //true - lose comparison - checking only the value
+console.log(x === '1') //false - strict comparison - checking both value and data type. 1 as number is not equal to 1 as a string
