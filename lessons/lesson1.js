89b248a8ebf78e00742db4d6ee39b3b0eb9005bd
DominiqueOyco <dominickoyco@gmail.com>
2024-10-01 12:58:35 -0700

    Add lessons 1 and 2

---
+++ lessons/lesson1.js
@@ -0,0 +1,30 @@
+// 1. Hello World
+console.log("Hello World!")
+
+// Variables - can be reassigned
+var firstName = "John"
+let lastName = "Smith"
+console.log(firstName)
+console.log(lastName)
+console.log(firstName, lastName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age)
+age = "6"
+console.log(age)
+
+//constants
+const occupation = "Engineer" //const must be initialized or else there would be an error
+console.log(occupation)
+occupation = "driver"
+console.log(occupation)
+
+//data types
+var middleName = "David" //string - '' or ""
+var ageOfBrother = 25 //number
+var isHeMarried = false //boolean 
+var yearsInMarriage = null //null 
+var numberOfCars = undefined //undefined - cannot use
+
---
+++ lessons/lesson2.js
@@ -0,0 +1,7 @@
+//Concatenation and Interpolation
+var price = 80
+var itemName = "Table"
+var messageToPrint = "The price for your " + itemName + " is " + price + " dollars" //concatenation
+var messageToPrint2 = `The price for your ${itemName} is ${price} dollars` //interpolation
+console.log(messageToPrint)
+console.log(messageToPrint2)
